7ed074d156a5173abc37f6a8163e8e178e69acab
ziga50 <148458200+ziga50@users.noreply.github.com>
2024-02-29 08:21:35 +0100

    Update graf_krevs_23.py

--- graf_krevs_23.py
+++ graf_krevs_23.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv(r"C:\Users\zigao\OneDrive - Univerza v Ljubljani\Documents\krevs_23_suša.csv")
 
-# Plotting
+
 plt.figure(figsize=(15, 12))
 plt.plot(df['Leto2'], df['km22'], marker='o', linestyle='-', color='b', label='podatki časovne vrste')
 plt.title('Obseg suše po letih', weight = "bold")
